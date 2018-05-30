c36eb2f56cdb5addf2af74d5c5d9de1095c39053
Rocky Chung <rckychng@gmail.com>
2018-05-30 17:29:55 -0400

    Initial Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,40 @@
 const gulp = require('gulp');
-const babel = require('babelify');
-const browserify = require('browserify');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
-const notify = require('gulp-notify');
 const sass = require('gulp-sass');
-const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
 gulp.task('styles', () => {
 	return gulp.src('./dev/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
 		.pipe(concat('style.css'))
 		.pipe(gulp.dest('./public/styles'))
+		.pipe(reload({ stream: true }));
 });
 
-gulp.task('js', () => {
-	return browserify('dev/scripts/app.js', {debug: true})
-		.transform('babelify', {
-			sourceMaps: true,
-			presets: ['env','react']
-		})
-		.bundle()
-		.on('error',notify.onError({
-			message: "Error: <%= error.message %>",
-			title: 'Error in JS 💀'
-		}))
-		.pipe(source('app.js'))
-		.pipe(buffer())
-		.pipe(gulp.dest('public/scripts'))
-		.pipe(reload({stream:true}));
+gulp.task('scripts', () => {
+    return gulp.src('./dev/scripts/main.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('./public/scripts'))
+        .pipe(reload({ stream: true }));
 });
 
-gulp.task('bs', () => {
-	return browserSync.init({
+gulp.task('browser-sync', () => {
+	browserSync.init({
 		server: {
 			baseDir: './'
 		}
 	});
 });
 
-gulp.task('default', ['bs','js','styles'], () => {
-	gulp.watch('dev/**/*.js',['js']);
+gulp.task('default', ['browser-sync','scripts','styles'], () => {
+	gulp.watch('dev/**/*.js',['scripts']);
 	gulp.watch('dev/**/*.scss',['styles']);
 	gulp.watch('./public/styles/style.css',reload);
+	gulp.watch('*.html', reload);
 });
