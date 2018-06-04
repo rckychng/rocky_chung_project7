66a32a8b532034fc92e8227ffb5e362b59f782ea
Rocky Chung <rckychng@gmail.com>
2018-06-03 23:27:59 -0400

    Styling updates

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -4,6 +4,9 @@ myApp.navBarToggle = () => {
     $(".header__icon").on("click",() => {
         $(".header__nav").toggleClass("header__nav--active")
     });
+    $(".header__icon").on("mouseover", () => {
+        $(".header__nav").toggleClass("header__nav--active")
+    });
 }
 
 myApp.navBarRemove = () => {
@@ -12,11 +15,16 @@ myApp.navBarRemove = () => {
     });
 }
 
+myApp.scroll = () => {
+    $(".header__link-text").smoothScroll();
+}
+
 // INITIALIZE
 
 myApp.init = () => {
     myApp.navBarToggle()
     myApp.navBarRemove()
+    myApp.scroll()
 };
 
 // DOCUMENT READY
