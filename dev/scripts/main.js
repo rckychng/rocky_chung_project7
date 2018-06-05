17ad1fbd4e8f868b68741da00f84c3e5641e1b30
Rocky Chung <rckychng@gmail.com>
2018-06-05 13:01:10 -0400

    minor design changes

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -15,16 +15,57 @@ myApp.navBarRemove = () => {
     });
 }
 
-myApp.scroll = () => {
-    $(".header__link-text").smoothScroll();
+// myApp.scroll = () => {
+//     $(".header__link-text").smoothScroll();
+// }
+
+myApp.smoothScroll = () => {
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+            ) {
+            // Figure out element to scroll to
+            let target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                scrollTop: target.offset().top
+                }, 1000, function () {
+                // Callback after animation
+                // Must change focus!
+                const $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { // Checking if the target was focused
+                    return false;
+                } else {
+                    $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                    $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
 }
+
+
 
 // INITIALIZE
 
 myApp.init = () => {
     myApp.navBarToggle()
     myApp.navBarRemove()
-    myApp.scroll()
+    // myApp.scroll()
+    myApp.smoothScroll()
 };
 
 // DOCUMENT READY
